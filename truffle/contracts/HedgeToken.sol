5f84286d6c1fc41ca025e4c8bd80f9b9ca45c089
Matej Nemček <ybdaba@gmail.com>
2020-11-04 13:53:15 +0100

    truffle tutorial ready

--- truffle/contracts/HedgeToken.sol
+++ truffle/contracts/HedgeToken.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract HedgeToken is ERC20 {
     constructor(uint256 initialSupply) public ERC20("HedgeToken", "HEDGE") {
